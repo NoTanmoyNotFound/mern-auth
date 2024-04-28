0563fc34e23318792995ad511c646bb92dd2887b
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2024-04-28 13:36:39 +0530

    add routes and create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+// import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Profile from './Pages/Profile';
+import SignUp from './Pages/SignUp';
+import SignIn from './Pages/SignIn';
 
 function App() {
   return (
-    <div>
-      <h1 className='text-red-600'>Hello world </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/Profile' element={<Profile />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/sign-in' element={<SignIn />} />
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 
