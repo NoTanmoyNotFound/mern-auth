cd6224f707b2ecbfcc7bb70005d31e4a3ad7d9cb
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2024-05-19 10:30:54 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
 import express from "express";
+import mongoose, { connect } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log("Connected to MongoDB");
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+
+
+
 
 const app = express();
 
