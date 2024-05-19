de09265d7d7c168e2a8a463764493ca7b232a22f
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2024-05-19 11:12:03 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose, { connect } from "mongoose";
 import dotenv from "dotenv";
+
+import userRoutes from "./routes/user.route.js";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -20,3 +22,11 @@ const app = express();
 app.listen(3000,()=>{
     console.log("Listening on port 3000");
 })
+
+//test api to route
+// app.get("/", (req, res) => {
+//   res.json({ message: "Api is running" });
+// })
+
+app.use('/',userRoutes);
+app.use('/api/user',userRoutes);
