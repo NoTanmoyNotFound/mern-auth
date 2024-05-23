4e19b03a88496c37f5a644cedf07ba9ce4f78218
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2024-05-23 14:24:53 +0530

    nodemon crashed i'm dying

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,8 @@
-export const signup = (req, res) => {
-    console.log(req.body);
+import User from "../api/models/user.model";
+
+export const signup = async (req, res) => {
+    const { username, email, password } = req.body;
+    const newUser = new User({ username, email, password });
+    await newUser.save()
+    res.status(201).json({message : "User created successfully"});
 };
