a94156001d2581323222ff01a7ad9bac4bd965ec
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2024-06-22 11:20:35 +0530

    fixing importing issue

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
-        unique: true
+        
     },
     email: {
         type: String,
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,8 @@
-import User from "../api/models/user.model";
+import User from "../api/models/user.model.js";
 
 export const signup = async (req, res) => {
-    const { username, email, password } = req.body;
-    const newUser = new User({ username, email, password });
+    const { name, email, password } = req.body;
+    const newUser = new User({ name, email, password });
     await newUser.save()
     res.status(201).json({message : "User created successfully"});
 };
