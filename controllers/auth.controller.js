6ec4c31aa9162af7e71242978676517fa0802638
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2024-06-24 11:25:34 +0530

    create sign up api route

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,17 @@
 import User from "../api/models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res) => {
     const { name, email, password } = req.body;
-    const newUser = new User({ name, email, password });
-    await newUser.save()
-    res.status(201).json({message : "User created successfully"});
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({ name, email, password: hashedPassword });
+    try {
+        await newUser.save()
+        res.status(201).json({message : "User created successfully"});
+        
+    } catch (error) {
+        res.status(500).json({message : error.message});
+    }
+    
+    
 };
