40833d57e445c5bcf59042f610a73f34c7557b24
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2024-06-28 14:26:29 +0530

    complete sign up page functionality

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -3,6 +3,9 @@ import {Link} from 'react-router-dom';
 
 function SignUp() {
   const [formData, setFormData] = useState({})
+  const [error , setError] = useState(false);
+  const [loading , setLoading] = useState(false);
+
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value})
   }
@@ -10,15 +13,29 @@ function SignUp() {
   
   const handleSubmit = async(e) => {
     e.preventDefault();
-    const res = await fetch('/api/auth/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    });
-    const data = await res.json(); 
-    console.log(data);
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json(); 
+      console.log(data);
+      setLoading(false);
+      if(data.success === false){
+        setError(true);
+        return;
+      }
+      ;
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+    
   };
    
   
@@ -29,7 +46,7 @@ function SignUp() {
         <input type="text" placeholder='Username' id='username' className='bg-slate-100 p-3 rounded-lg ' onChange={handleChange}/>
         <input type="text" placeholder='Email' id='email' className='bg-slate-100 p-3 rounded-lg ' onChange={handleChange}/>
         <input type="password" placeholder='Password' id='password' className='bg-slate-100 p-3 rounded-lg 'onChange={handleChange} />
-        <button className=' bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-80 '>Sign Up</button>
+        <button disabled={loading} className=' bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-80 '>{loading ? "Loading..." : "Sign Up"}</button>
       </form>
       <div className='flex gap-3 mt-5'>
         <p>Have an account?</p>
@@ -37,6 +54,7 @@ function SignUp() {
         <span className='text-blue-500 font-semibold cursor-pointer'>Sign in</span>
         </Link>
       </div>
+      <p className='text-red-700 mt-5'>{error && "Something went wrong"}</p>
     </div>
   )
 }
