e4c8d39134be08497f9a968d376e2ce322752164
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2024-06-29 11:00:01 +0530

    create sign in api fr

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,7 @@ import User from "../api/models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../api/utils/error.js";
 import Ok from "../api/models/ok.model.js";
+import  Jwt  from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -16,5 +17,26 @@ export const signup = async (req, res, next) => {
         console.log(error);
         next(error);
         // next(errorHandler(300,"Something went wrong")); if you want to show custom error but i dont need it 
+    }
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({email});
+        if (!validUser) return next(errorHandler(404, "User not found"));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, "Invalid credentials"));
+        const token = Jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+        const {password: hashedPassword, ...rest} = validUser._doc;
+
+        const expiryDate = new Date();
+        expiryDate.setDate(expiryDate.getDate() + 1); // Set the expiry date to 1 day from now
+
+        res.cookie('access_token', token, {httpOnly: true, expires: expiryDate})
+            .status(200)
+            .json(rest);
+    } catch (error) {
+        next(error);
     }
 };
